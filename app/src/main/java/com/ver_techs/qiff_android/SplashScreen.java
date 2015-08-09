4e32b29889d836b2b11d0ac40b44bc2204ede9ab
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-08-09 20:37:53 +0530

    Add Comments to SplashScreen.java

--- app/src/main/java/com/ver_techs/qiff_android/SplashScreen.java
+++ app/src/main/java/com/ver_techs/qiff_android/SplashScreen.java
@@ -20,19 +20,15 @@ public class SplashScreen extends Activity {
 
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+            //Showing splash screen with a timer.
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
+                // Executed once the timer is over, start main activity
                 Intent i = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(i);
 
-                // close this activity
+                // Close this activity
                 finish();
             }
         }, SPLASH_TIME_OUT);

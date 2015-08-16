e8c964f2f4c8027fe1cd64402246de8c92bedda5
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-08-16 22:08:41 +0530

    Add ViewPagerAdapter.java, Tab2.java and modify SlidingtabLayout.java

---
+++ app/src/main/java/com/ver_techs/qiff_android/Tab2.java
@@ -0,0 +1,20 @@
+package com.ver_techs.qiff_android;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Edwin on 15/02/2015.
+ */
+public class Tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.activity_main,container,false);
+        return v;
+    }
+}

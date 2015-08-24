42f81150b5498be21895300ce76ad2afb38c95f8
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-08-24 22:50:08 +0530

    Add Basic calendar

--- app/src/main/java/com/ver_techs/qiff_android/CalendarFragment.java
+++ app/src/main/java/com/ver_techs/qiff_android/CalendarFragment.java
@@ -6,15 +6,119 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.GregorianCalendar;
 
-/**
- * Created by Edwin on 15/02/2015.
- */
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 public class CalendarFragment extends Fragment {
+
+    public GregorianCalendar cal_month, cal_month_copy;
+    private CalendarAdapter cal_adapter;
+    private TextView tv_month;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.fragment_tab_1,container,false);
+        View v =inflater.inflate(R.layout.fragment_calendar,container,false);
+
+        cal_month = (GregorianCalendar) GregorianCalendar.getInstance();
+        cal_month_copy = (GregorianCalendar) cal_month.clone();
+        cal_adapter = new CalendarAdapter( getActivity(), cal_month, CalendarCollection.date_collection_arr);
+
+
+
+        tv_month = (TextView) v.findViewById(R.id.tv_month);
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
+
+        ImageButton previous = (ImageButton) v.findViewById(R.id.ib_prev);
+
+        previous.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setPreviousMonth();
+                refreshCalendar();
+            }
+        });
+
+        ImageButton next = (ImageButton) v.findViewById(R.id.Ib_next);
+        next.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setNextMonth();
+                refreshCalendar();
+
+            }
+        });
+
+        GridView gridview = (GridView) v.findViewById(R.id.gv_calendar);
+        gridview.setAdapter(cal_adapter);
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v,position);
+                String selectedGridDate = CalendarAdapter.day_string
+                        .get(position);
+
+                String[] separatedTime = selectedGridDate.split("-");
+                String gridvalueString = separatedTime[2].replaceFirst("^0*","");
+                int gridvalue = Integer.parseInt(gridvalueString);
+
+                if ((gridvalue > 10) && (position < 8)) {
+                    setPreviousMonth();
+                    refreshCalendar();
+                } else if ((gridvalue < 7) && (position > 28)) {
+                    setNextMonth();
+                    refreshCalendar();
+                }
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v,position);
+
+                ((CalendarAdapter) parent.getAdapter()).getPositionList(selectedGridDate, getActivity());
+            }
+
+        });
+
         return v;
+
     }
+
+
+    protected void setNextMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month
+                .getActualMaximum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) + 1),
+                    cal_month.getActualMinimum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) + 1);
+        }
+
+    }
+
+    protected void setPreviousMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month
+                .getActualMinimum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) - 1),
+                    cal_month.getActualMaximum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) - 1);
+        }
+
+    }
+
+    public void refreshCalendar() {
+        cal_adapter.refreshDays();
+        cal_adapter.notifyDataSetChanged();
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
+    }
+
 }
+

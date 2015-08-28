996e0dd4319d37167b76cacfef02e476a80b0756
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-08-28 22:28:43 +0530

    Add getters and setters to ChatItem.java class

--- app/src/main/java/com/ver_techs/qiff_android/ChatItem.java
+++ app/src/main/java/com/ver_techs/qiff_android/ChatItem.java
@@ -8,5 +8,44 @@ import com.parse.ParseClassName;
  */
 
 public class ChatItem extends ParseObject{
-}
 
+    String userName;
+    String chatMessage;
+
+    //deafult constructor
+    public  ChatItem(){
+    }
+
+    public ChatItem(String username, String chatmessage) {
+
+        super();
+        userName=username;
+        chatMessage=chatmessage;
+
+    }
+
+    public void setUserName(String username){
+
+        userName=username;
+
+    }
+
+    public void setChatMessage(String chatmessage){
+
+        chatMessage=chatmessage;
+
+    }
+
+    public String getUserName() {
+
+        return userName;
+
+    }
+
+    public String getChatMessage() {
+
+        return chatMessage;
+
+    }
+
+}

f50f2f5a05172222ff26bb2cbfa1fe8ea418a2c1
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-08-28 22:03:35 +0530

    Add empty class file ChatItem.java

---
+++ app/src/main/java/com/ver_techs/qiff_android/ChatItem.java
@@ -0,0 +1,12 @@
+package com.ver_techs.qiff_android;
+
+import com.parse.ParseObject;
+import com.parse.ParseClassName;
+
+/**
+ * Created by Sony on 8/28/2015.
+ */
+
+public class ChatItem extends ParseObject{
+}
+

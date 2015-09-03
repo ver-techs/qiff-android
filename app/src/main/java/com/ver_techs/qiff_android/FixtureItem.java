37d9aa4fcae97071c337fd7aaad090d1bfa4c2e8
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-09-04 00:16:15 +0530

    Add getters to FixtureItem class

--- app/src/main/java/com/ver_techs/qiff_android/FixtureItem.java
+++ app/src/main/java/com/ver_techs/qiff_android/FixtureItem.java
@@ -1,7 +1,48 @@
 package com.ver_techs.qiff_android;
 
+import com.parse.ParseObject;
+import com.parse.ParseClassName;
+
 /**
- * Created by Sony on 9/4/2015.
+ * Created by Sony on 8/28/2015.
  */
-public class FixtureItem {
+
+@ParseClassName("FixtureItem")
+public class FixtureItem extends ParseObject{
+
+    String teamName1, teamName2;
+    String scoreTeam1, scoreTeam2;
+    String timeDate;
+
+    //default constructor
+    public  FixtureItem(){
+    }
+
+    public String getTeamName1() {
+
+        return getString("teamName1");
+    }
+
+    public String getTeamName2() {
+
+        return getString("teamName2");
+
+    }
+
+    public String getScoreTeam1() {
+
+        return getString("scoreTeam1");
+    }
+
+    public String getScoreTeam2() {
+
+        return getString("scoreTeam2");
+
+    }
+
+    public String getTimeDate() {
+
+        return getString("timeDate");
+    }
+
 }

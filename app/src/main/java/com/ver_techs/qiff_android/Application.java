e639d96d71e85e79743ab9a8da312db7f6592d1d
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-09-12 15:44:31 +0530

    Subscribe to Parse channel QIFF in Application.java

--- app/src/main/java/com/ver_techs/qiff_android/Application.java
+++ app/src/main/java/com/ver_techs/qiff_android/Application.java
@@ -1,7 +1,12 @@
 package com.ver_techs.qiff_android;
 
+import android.util.Log;
+
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 /**
  * Created by Sony on 9/9/2015.
@@ -14,5 +19,12 @@ public class Application extends android.app.Application {
 
         Parse.initialize(this, "2WLxFTr9dd04QQf8zClDnwgqvGeKPhmm9f03iaBB", "a4Ac6BAISHDRGU70dxc1mHgjXFfD1vPTyOvCV3DA");
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        ParsePush.subscribeInBackground("QIFF", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                Log.i("aaki", "Successfully subscribed to Parse!");
+            }
+        });
     }
 }

28cec75c81946cb83eea634c0e785404a0fb4d30
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-09-12 15:55:23 +0530

    Recieve json push message using CustomPushReciever, parse it and start a new intent

--- app/src/main/java/com/ver_techs/qiff_android/CustomPushReciever.java
+++ app/src/main/java/com/ver_techs/qiff_android/CustomPushReciever.java
@@ -1,7 +1,73 @@
 package com.ver_techs.qiff_android;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.parse.ParsePushBroadcastReceiver;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
- * Created by Sony on 9/12/2015.
+ * Created by Ravi on 01/06/15.
  */
-public class CustomPushReciever {
+
+public class CustomPushReceiver extends ParsePushBroadcastReceiver {
+
+    private Intent parseIntent;
+
+    public CustomPushReceiver() {
+        super();
+    }
+
+    @Override
+    protected void onPushReceive(Context context, Intent intent) {
+        super.onPushReceive(context, intent);
+
+        if (intent == null)
+            return;
+
+        try {
+            JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+
+            Log.i("aaki", "Push received: " + json);
+
+            parseIntent = intent;
+
+            parsePushJson(context, json);
+
+        } catch (JSONException e) {
+            Log.e("aaki", "Push message json exception: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onPushDismiss(Context context, Intent intent) {
+        super.onPushDismiss(context, intent);
+    }
+
+    @Override
+    protected void onPushOpen(Context context, Intent intent) {
+        super.onPushOpen(context, intent);
+    }
+
+    /**
+     * Parses the push notification json
+     */
+
+    private void parsePushJson(Context context, JSONObject json) {
+        try {
+
+            JSONObject data = json.getJSONObject("data");
+            String title = data.getString("team1");
+            String message = data.getString("team2");
+
+            Intent resultIntent = new Intent(context, MainActivity.class);
+
+        } catch (JSONException e) {
+            Log.e("aaki", "Push message json exception: " + e.getMessage());
+        }
+    }
+
 }

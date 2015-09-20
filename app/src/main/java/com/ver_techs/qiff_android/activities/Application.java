3f6cb52b8bf6858f2c5cb13ff5e92605d417017a
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-09-20 13:30:06 +0530

    Remove parse channel subscription

--- app/src/main/java/com/ver_techs/qiff_android/activities/Application.java
+++ app/src/main/java/com/ver_techs/qiff_android/activities/Application.java
@@ -18,11 +18,5 @@ public class Application extends android.app.Application {
         Parse.initialize(this, "2WLxFTr9dd04QQf8zClDnwgqvGeKPhmm9f03iaBB", "a4Ac6BAISHDRGU70dxc1mHgjXFfD1vPTyOvCV3DA"); //Initializing parse
         ParseInstallation.getCurrentInstallation().saveInBackground(); //Getting installation object for parse push
 
-        ParsePush.subscribeInBackground("QIFF", new SaveCallback() {
-            @Override
-            public void done(ParseException e) { //subscribing to channel QIFF for parse push
-                Log.i("aaki", "Successfully subscribed to Parse channel QIFF !");
-            }
-        });
     }
 }

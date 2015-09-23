54f10992b4a8d3b7523b91a8bf41a5f05b0b6849
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-09-23 22:12:17 +0530

    Add swiperefresh to fixturefragment - not working

--- app/src/main/java/com/ver_techs/qiff_android/fragments/FixtureFragment.java
+++ app/src/main/java/com/ver_techs/qiff_android/fragments/FixtureFragment.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,11 +24,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 //Fixture fragment that contains fixture view
-public class FixtureFragment extends Fragment {
+public class FixtureFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private ProgressDialog nDialog; //progress dialog to show while parse query is running in background
     ArrayList<FixtureItemLocal> fixtureItemArrayList; //list of fixture items, loaded from parse
     View v;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    ListView fixtureList;
+    FixtureCustomAdapter fixtureListAdapter;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
@@ -39,9 +43,19 @@ public class FixtureFragment extends Fragment {
         nDialog.setCancelable(false);
         nDialog.show();
 
+        fixtureList = (ListView) v.findViewById(R.id.list_fixture); //find the listview to load fixture items
+        fixtureListAdapter = new FixtureCustomAdapter(getActivity(), fixtureItemArrayList); //get a new istance of adapter for fixture view
+        fixtureItemArrayList = new ArrayList<FixtureItemLocal>();
+        swipeRefreshLayout = (SwipeRefreshLayout) getActivity().findViewById(R.id.swipe_refresh_layout);
+
         setRetainInstance(true); //configure the fragment instance to be retained on configuration change
 
-        fixtureItemArrayList = new ArrayList<FixtureItemLocal>();
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         // Define the class we would like to query
         ParseQuery<FixtureItem> query = ParseQuery.getQuery(FixtureItem.class);
@@ -63,8 +77,6 @@ public class FixtureFragment extends Fragment {
                     //Log.i("aaki", "task doing " + Integer.toString(fixtureItemArrayList.size()));
 
                     nDialog.cancel();
-                    FixtureCustomAdapter fixtureListAdapter = new FixtureCustomAdapter(getActivity(), fixtureItemArrayList); //get a new istance of adapter for fixture view
-                    ListView fixtureList = (ListView) v.findViewById(R.id.list_fixture); //find the listview to load fixture items
                     fixtureList.setAdapter(fixtureListAdapter); //set the adapter to the listview
 
                 } else {
@@ -73,7 +85,11 @@ public class FixtureFragment extends Fragment {
             }
         });
 
-        return v;
+        swipeRefreshLayout.setOnRefreshListener(this);
     }
 
+    @Override
+    public void onRefresh() {
+        Log.i("aaki", "refresh");
+    }
 }

d2fedb417399e58018716699a946f3ca5e811c32
Abdul Jaleel <ajaleelp@gmail.com>
2015-09-30 01:21:49 +0530

    POC: Broadcast a notification if FixtureItem changes

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,6 +1,16 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.afterSave("FixtureItem", function(request) {
+  var pushQuery = new Parse.Query(Parse.Installation);
+  Parse.Push.send({
+      where: pushQuery,
+      data: {
+          alert: "Someone just updated the fixture!"
+      }
+  }, {
+      success: function() {
+          // Push successful
+      },
+      error: function(error) {
+          throw "Got an error " + error.code + " : " + error.message;
+      }
+  });
 });

d6f6e93fbbcc28d93881ba559aa4cc7d31eb6a40
AAKIFAH RAHMAN <aakifahrahiman@yahoo.co.in>
2015-10-24 11:32:00 +0530

    FanZone code clean up, resizefan chat layouts

--- app/src/main/java/com/ver_techs/qiff_android/activities/FanZone.java
+++ app/src/main/java/com/ver_techs/qiff_android/activities/FanZone.java
@@ -37,10 +37,11 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+//Activity class to display fan chats and allow user to send fan chats
 public class FanZone extends Activity {
 
     EditText message_editText;
-    String userName = "nothing", message;
+    String userName, message;
     Boolean touchedOnce = false; //boolean to determine if chatbox is being focused on for first time or not
     Typeface custom_font;
     ChatCustomAdapter chatCustomAdapter;
@@ -55,7 +56,6 @@ public class FanZone extends Activity {
         TextView fan_zone = (TextView) findViewById(R.id.fan_zone);
         EditText message_box = (EditText) findViewById(R.id.message_box);
 
-
         listView = (ListView) findViewById(R.id.msgview);
         chatItemArrayList = new ArrayList<ChatItemLocal>();
 
@@ -66,7 +66,7 @@ public class FanZone extends Activity {
 
         message_box.setHintTextColor(getResources().getColor(R.color.color_secondary));
 
-        //Code to check if user has focused on message edittext, if so, if there is no username yet, get from user
+        //Code to check if user has focused on message edittext, if so, allow keyboard to pop up
 
         message_editText = (EditText) findViewById(R.id.message_box);
 
@@ -79,18 +79,17 @@ public class FanZone extends Activity {
             }
         });
 
-        //Chat send button code - to get username first time, save as sharedpref, use it subsequently, and send chat messages
+        //Chat send button code - to send chat messages
 
         Button send_chat_button = (Button) findViewById(R.id.send_chat_button);
         send_chat_button.setOnClickListener(new View.OnClickListener() {
 
                                                 @Override
                                                 public void onClick(View v) {
-                //send chat with existing User Name
-                sendChatToParse();
+                    //send chat with existing User Name
+                    sendChatToParse();
+                }
             }
-        }
-
         );
 
         updateFanZoneWithParseChats();
@@ -110,7 +109,7 @@ public class FanZone extends Activity {
                         // When fields have been recieved
 
                         try {
-                            userName = object.getString("first_name");
+                            userName = object.getString("first_name"); //get first name from the json object
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -118,7 +117,7 @@ public class FanZone extends Activity {
                         message = message_editText.getText().toString(); //get message from text box
 
                         if(!message.equals("")) {
-                            ChatItem chatItem = new ChatItem(userName, message); //create a new chatitem
+                            ChatItem chatItem = new ChatItem(userName, message); //create a new chatitem with username and message
 
                             // Save the data to Parse whenever internet is available
                             chatItem.saveInBackground(new SaveCallback() {
@@ -137,9 +136,9 @@ public class FanZone extends Activity {
                 });
 
         Bundle parameters = new Bundle();
-        parameters.putString("fields", "first_name");
+        parameters.putString("fields", "first_name"); //fields to fetch during facebook api request
         request.setParameters(parameters);
-        request.executeAsync();
+        request.executeAsync(); //execute api call
 
     }
 
@@ -161,7 +160,7 @@ public class FanZone extends Activity {
                     // Access the array of results here
                     for (int i = chatItemList.size() - 1; i >= 0; i--) {
 
-                        if((i % 2) == 0) {
+                        if((i % 2) == 0) { //alternate chats are shown on left and right side of the screen
                             ChatItemLocal chatItemLocal = new ChatItemLocal(chatItemList.get(i).getUserName(), chatItemList.get(i).getChatMessage(), "right");
                             chatItemArrayList.add(chatItemLocal);
                         }
@@ -182,7 +181,7 @@ public class FanZone extends Activity {
     }
 
     @Override
-    public void onBackPressed(){
+    public void onBackPressed(){ //on back button being pressed, move to main activity
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);

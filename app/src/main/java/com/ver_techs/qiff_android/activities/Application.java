97da59ffebbe4714620791983e61b8bac5a07142
Aakifah Rahman <aakifahrahman@gmail.com>
2016-03-14 23:10:08 +0530

    Testing

--- app/src/main/java/com/ver_techs/qiff_android/activities/Application.java
+++ app/src/main/java/com/ver_techs/qiff_android/activities/Application.java
@@ -12,7 +12,7 @@ public class Application extends android.app.Application {
         super.onCreate();
 
         Parse.initialize(this, "2WLxFTr9dd04QQf8zClDnwgqvGeKPhmm9f03iaBB", "a4Ac6BAISHDRGU70dxc1mHgjXFfD1vPTyOvCV3DA"); //Initializing parse
-        ParseInstallation.getCurrentInstallation().saveInBackground(); //Getting installation object for parse push
+        ParseInstallation.getCurrentInstallation().saveInBackground(); // Getting installation object for parse push
 
         FacebookSdk.sdkInitialize(getApplicationContext()); // Initialize the Facebook SDK
     }

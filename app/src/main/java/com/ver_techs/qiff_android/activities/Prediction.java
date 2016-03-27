385427a42a375c130a4acd849642d316487ef225
Aakifah Rahman <aakifahrahman@gmail.com>
2016-03-27 10:15:49 +0530

    Add new dimension values to dimens.xml

--- app/src/main/java/com/ver_techs/qiff_android/activities/Prediction.java
+++ app/src/main/java/com/ver_techs/qiff_android/activities/Prediction.java
@@ -3,6 +3,7 @@ package com.ver_techs.qiff_android.activities;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
@@ -22,7 +23,7 @@ import java.util.List;
 /**
  * Created by aakif on 14-03-2016.
  */
-public class Prediction extends ActionBarActivity {
+public class Prediction extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +38,24 @@ public class Prediction extends ActionBarActivity {
         Typeface custom_font = Typeface.createFromAsset(getAssets(), getString(R.string.font_path));
         prediction_text.setTypeface(custom_font, Typeface.BOLD);
 
-        final TextView question = (TextView) findViewById(R.id.question);
-
-        ParseQuery query = ParseQuery.getQuery("PredictionQuestions");
-        query.findInBackground(new FindCallback<PredictionQuestions>() {
-
-            public void done(List<PredictionQuestions> predictionQuestionsList, ParseException e) {
-
-                if (e == null) {
-                    // Access the array of results here
-                    for (int i = 0; i < predictionQuestionsList.size(); i++) {
-                        PredictionQuestionsLocal predictionQuestionsLocal = new PredictionQuestionsLocal(
-                                predictionQuestionsList.get(i).getString("question"), predictionQuestionsList.get(i).getString("matchId"));
-                        question.setText(predictionQuestionsList.get(i).getString("question"));  //set team names
-                    }
-
-                }
-            }
-        });
+//        final TextView question = (TextView) findViewById(R.id.question);
+//
+//        ParseQuery query = ParseQuery.getQuery("PredictionQuestions");
+//        query.findInBackground(new FindCallback<PredictionQuestions>() {
+//
+//            public void done(List<PredictionQuestions> predictionQuestionsList, ParseException e) {
+//
+//                if (e == null) {
+//                    // Access the array of results here
+//                    for (int i = 0; i < predictionQuestionsList.size(); i++) {
+//                        PredictionQuestionsLocal predictionQuestionsLocal = new PredictionQuestionsLocal(
+//                                predictionQuestionsList.get(i).getString("question"), predictionQuestionsList.get(i).getString("matchId"));
+//                        question.setText(predictionQuestionsList.get(i).getString("question"));  //set team names
+//                    }
+//
+//                }
+//            }
+//        });
     }
 
             @Override
